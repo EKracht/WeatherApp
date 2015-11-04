1fc979d70a1529e14f810aaec1e88018b6e9c30b
EKracht <ethan1kracht@gmail.com>
2015-11-04 01:10:32 -0800

    This git will clean up console.log's and add animation

--- main.js
+++ main.js
@@ -1,60 +1,27 @@
 'use script';
 
-// http://api.wunderground.com/api/eb6417bed27cef58/    forecast/q/CA/San_Francisco.json
 var apiUrl = "http://api.wunderground.com/api/eb6417bed27cef58";
 
 $(document).ready(init);
 
 function init(){
-  //starting weather
   var startCur = apiUrl + "/conditions/q/autoip.json";
   var start = apiUrl + "/forecast/q/autoip.json";
+    
+  $.get(startCur)
+  .done(function(data){
+    makeCurrentCard(data);
+  })
+  .fail(function(error){
+  })
 
   $.get(start)
   .done(function(data){
-    console.log(data);
-
-  for (var i = 1; i <= 7; i = i + 2){
-    var dataFrom = data.forecast.txt_forecast.forecastday[i];
-    var $card = $('<div>').addClass('card');
-    var $day = $('<div>').text(dataFrom.title);
-    console.log('day', $day);
-    var $image = $('<img>').attr('src', dataFrom.icon_url);
-    console.log('image', $image);
-    var $forecast = $('<div>').text('Forecast: ' + dataFrom.fcttext);
-    console.log('forecast', $forecast);
-    $card.append($day, $image, $forecast);
-    console.log('card', $card);
-    console.log('i', i);
-    $('.futureWeather').append($card);
-  }
+    makeCards(data);
   })
   .fail(function(error){
-    console.error(error);
-  })
-  
-  $.get(startCur)
-  .done(function(data){
-    console.log(data);
-    var dataFromCur = data.current_observation;
-    var $card = $('<div>').addClass('specialCard');
-    var $location = $('<div>').text(dataFromCur.display_location.full);
-    var $day = $('<div>').text("Today");
-    console.log('day', $day);
-    var $image = $('<img>').attr('src', dataFromCur.icon_url);
-    var $temperature = $('<div>').text('Temperature: ' + dataFromCur.temperature_string);
-    var $weather = $('<div>').text('Weather: ' + dataFromCur.weather);
-    var $wind = $('<div>').text('Wind: ' + dataFromCur.wind_string);
-    console.log('forecast', $temperature);
-    $card.append($location, $day, $image, $temperature, $weather, $wind);
-    console.log('card', $card);
-    $('.currentWeather').append($card);
-  })
-  .fail(function(error){
-    console.error(error);
   })
 
-  //Event Listeners
   $('#getWeather').click(getWeather);
 }
 
@@ -62,50 +29,69 @@ function getWeather(){
   $('.card').remove();
   $('.specialCard').remove();
   var $input = $('#zipCode').val();
-  var urlForecast = apiUrl + '/forecast/q/' + $input + '.json';
   var urlConditions = apiUrl + '/conditions/q/' + $input + '.json';
-
+  var urlForecast = apiUrl + '/forecast/q/' + $input + '.json';
+ 
   $.get(urlConditions)
   .done(function(data){
     console.log(data);
-    var dataFromCur = data.current_observation;
-    var $card = $('<div>').addClass('specialCard');
-    var $location = $('<div>').text(dataFromCur.display_location.full);
-    var $day = $('<div>').text("Today");
-    console.log('day', $day);
-    var $image = $('<img>').attr('src', dataFromCur.icon_url);
-    var $temperature = $('<div>').text('Temperature: ' + dataFromCur.temperature_string);
-    var $weather = $('<div>').text('Weather: ' + dataFromCur.weather);
-    var $wind = $('<div>').text('Wind: ' + dataFromCur.wind_string);
-    console.log('forecast', $temperature);
-    $card.append($location, $day, $image, $temperature, $weather, $wind);
-    console.log('card', $card);
-    $('.currentWeather').append($card);
+    makeCurrentCard(data);
   })
   .fail(function(error){
-    console.error(error);
   })
 
   $.get(urlForecast)
   .done(function(data){
-    console.log(data);
-
-  for (var i = 1; i <= 7; i = i + 2){
-    var dataFrom = data.forecast.txt_forecast.forecastday[i];
-    var $card = $('<div>').addClass('card');
-    var $day = $('<div>').text(dataFrom.title);
-    console.log('day', $day);
-    var $image = $('<img>').attr('src', dataFrom.icon_url);
-    console.log('image', $image);
-    var $forecast = $('<div>').text('Forecast: ' + dataFrom.fcttext);
-    console.log('forecast', $forecast);
-    $card.append($day, $image, $forecast);
-    console.log('card', $card);
-    console.log('i', i);
-    $('.futureWeather').append($card);
-  }
+    makeCards(data);
   })
   .fail(function(error){
-    console.error(error);
   })
 }
+
+function makeCurrentCard(data){
+  var dataFromCur = data.current_observation;
+  var $card = $('<div>').addClass('specialCard');
+  var $location = $('<div>').text(dataFromCur.display_location.full);
+  var $day = $('<div>').text("Today");
+  var $image = $('<img>').attr('src', dataFromCur.icon_url);
+  var $temperature = $('<div>').text('Temperature: ' + dataFromCur.temperature_string);
+  var $weather = $('<div>').text('Weather: ' + dataFromCur.weather);
+  var $wind = $('<div>').text('Wind: ' + dataFromCur.wind_string);
+  $card.append($location, $day, $image, $temperature, $weather, $wind);
+  $('.currentWeather').append($card);
+  $("img").rotate({ 
+   bind: 
+  { 
+    mouseover : function() { 
+        $(this).rotate({animateTo:180})
+    },
+    mouseout : function() { 
+        $(this).rotate({animateTo:0})
+    }
+  } 
+  });
+}
+
+function makeCards(data){
+  for (var i = 1; i <= 7; i = i + 2){
+    var dataFrom = data.forecast.txt_forecast.forecastday[i];
+    var $card = $('<div>').addClass('card col-lg-6');
+    var $day = $('<div>').text(dataFrom.title);
+    var $image = $('<img>').attr('src', dataFrom.icon_url);
+    var $forecast = $('<div>').text('Forecast: ' + dataFrom.fcttext);
+    $card.append($day, $image, $forecast);
+    $('.futureWeather').append($card);
+  }
+  $("img").rotate({ 
+    bind: 
+  { 
+    mouseover : function() { 
+        $(this).rotate({animateTo:180})
+    },
+    mouseout : function() { 
+        $(this).rotate({animateTo:0})
+    }
+  } 
+  });
+}
+
